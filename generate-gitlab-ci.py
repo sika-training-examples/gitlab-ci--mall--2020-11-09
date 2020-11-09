3027e001cc59311be515b4d4ac5947dd8a91f7df
Ondrej Sika <ondrej@ondrejsika.com>
2020-11-09 13:13:54 +0000

    Update .gitlab-ci.yml, generate-gitlab-ci.py files

---
+++ generate-gitlab-ci.py
@@ -0,0 +1,33 @@
+
+import json
+
+SERVICES = (
+    "foo",
+    "bar",
+    "baz",
+)
+
+def make_service(name):
+    return {
+        "build %s" % name: {
+            "stage": "build",
+            "script":[
+                "echo Build %s" % name,
+            ]
+        },
+        "deploy %s" % name: {
+            "stage": "deploy",
+            "script":[
+                "echo Deploy %s" % name,
+            ]
+        }
+    }
+
+with open(".gitlab-ci.generated.yml", "w") as f:
+    pipeline = {}
+    pipeline.update({
+        "stages": ["build", "deploy"]
+    })
+    for service in SERVICES:
+        pipeline.update(make_service(service))
+    f.write(json.dumps(pipeline))
